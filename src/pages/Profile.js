c0d536cdd119cf6bd48be2182ad5ac90af87e4fa
Lacto <>
2019-09-11 17:55:06 +0700

    change content for profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,9 +9,9 @@ export default class Profile extends Component {
         </header>
         <div class="row">
           <div data-animate="fadeInUp" class="col-lg-6">
-            <p>An sincerity so extremity he additions. Her yet <strong>there truth merit</strong>. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly. Off can admiration prosperous now devonshire diminution law.</p>
-            <p>Received overcame oh sensible so at an. Formed do change merely to county it. <strong>Am separate contempt</strong> domestic to to oh. On relation my so addition branched. Put hearing cottage she norland letters equally prepare too. Replied exposed savings he no viewing as up. Soon body add him hill. No father living really people estate if. Mistake do produce beloved demesne if am pursuit.</p>
-            <p>An sincerity so extremity he additions. Her yet <strong>there truth merit</strong>. Mrs all projecting favourable now unpleasing. Son law garden chatty temper. Oh children provided to mr elegance marriage strongly. Off can admiration prosperous now devonshire diminution law.</p>
+            <p>Let me introduce myself. My name is Alisa Buakao. My nickname is Oom. I Graduated from Kasetsart University where I studied Computer Engineering. I took  part in many activities at University such as TA and Staff of Camp. Also I was an intern at TRUE Coporate in the Web In House Developer team. I learned about coding and stucture of web. I interested in programming so that I did a final project about Web by PHP.</p>
+            <p>After I finished my degree, I started my career in IVR Developer team at GMM Grammy Company as a Programmer. After working there for 1 and a half years, I was to be JAVA developer in Billing Customer Service at TRUE Coporate. I have been working there for 4 years, I was to be Outsourcing Developer on various coporate such as AIS and TRUE Coporate. Currently I am a Full Stack Engineer at Emetworks on site at SCG.</p>
+            <p>About my personalities, I am a hard worker who work well with everyone. In my free time I enjoy to cooking and listening music. My English is not very good but I have been trying to improve it. I want to further my career. I’m looking for bigger challenges and new experiences.</p>
           </div>
           <div data-animate="fadeInUp" class="col-lg-6">
             <div class="skill-item">
